8b0c02e0fca08959e4ff97ed24da6010d2981ce0
Carlos Ortega <the_king_00carlos@hotmail.com>
2018-05-22 10:29:05 -0600

    Avance principal, hicimos parallax y animacion en el banner, construimos el sitio, falta responsive

--- gulpfile.js
+++ gulpfile.js
@@ -12,18 +12,26 @@ var autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify'),
     images = require('gulp-imagemin'),
+    babelify = require('babelify'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
     browserSync = require('browser-sync').create();
 
 
 // paths
 var styleSrc = 'source/sass/**/*.scss',
     styleDest = 'build/assets/css/',
+    bootSrc = 'node_modules/bootstrap/scss/bootstrap.scss',
     htmlSrc = 'source/',
     htmlDest = 'build/',
     vendorSrc = 'source/js/vendors/',
     vendorDest = 'build/assets/js/',
-    scriptSrc = 'source/js/*.js',
-    scriptDest = 'build/assets/js/';
+    scriptSrc = 'app.js',
+    jsFolder = 'source/js/',
+    scriptDest = 'build/assets/js/',
+    jsFILES = [scriptSrc];
 
 
 
@@ -34,13 +42,12 @@ var styleSrc = 'source/sass/**/*.scss',
 
 // Compiles all SASS files
 gulp.task('sass', function() {
-    gulp.src('source/sass/**/*.scss')
+    gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','source/sass/**/*.scss'])
         .pipe(plumber())
         .pipe(sass({
             style: 'compressed'
         }))
         .pipe(rename({
-            basename: 'main',
             suffix: '.min'
           }))
 
@@ -48,30 +55,54 @@ gulp.task('sass', function() {
 });
 
 gulp.task('images', function() {
-    gulp.src('source/img/*/*')
+    gulp.src(['source/img/*/*','source/img/*.jpg', 'source/img/*.png'])
         .pipe(images())
         .pipe(gulp.dest('build/assets/img'));
 });
 
 // Uglify js files
 gulp.task('scripts', function() {
-    gulp.src('source/js/*.js')
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
+
+    jsFILES.map(function( entry ){
+        return browserify({
+            entries: [jsFolder + entry]
+        })
+        .transform( babelify, {presets: ['env']} )
+        .bundle()
+        .pipe( source( entry ) )
+        .pipe( rename({ extname: '.min.js' }) )
+        .pipe( buffer() )
+        .pipe( sourcemaps.init({ loadMaps: true }) )
+        .pipe( uglify() )
+        .pipe( sourcemaps.write( './' ) )
+        .pipe( gulp.dest( scriptDest ) )
+    });
+
+    // gulp.src('source/js/*.js')
+    //     .pipe(plumber())
+    //     .pipe(uglify())
+    //     .pipe(gulp.dest('build/assets/js'));
 });
 
 //Concat and Compress Vendor .js files
+// --------------- Agragar scrollreveal y bootstrap, lo veremos con jesseshowalter ------------------
 gulp.task('vendors', function() {
     gulp.src(
             [
                 'source/js/vendors/jquery.min.js',
+                'source/js/vendors/fontawesome-all.min.js',
                 'source/js/vendors/*.js'
             ])
         .pipe(plumber())
         .pipe(concat('vendors.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('build/assets/js'));
+        .pipe(gulp.dest('build/assets/js/vendors'));
+});
+
+
+gulp.task('fonts', function(){
+    gulp.src(['source/fonts/*.otf'])
+        .pipe(gulp.dest('build/assets/fonts'));
 });
 
 
@@ -87,8 +118,8 @@ gulp.task('watch', function(){
         notify: false
     });
 
-    gulp.watch(styleSrc,['sass']);
-    gulp.watch(scriptSrc,['scripts']);
+    gulp.watch([bootSrc, styleSrc],['sass']);
+    gulp.watch(jsFolder+scriptSrc,['scripts']);
     gulp.watch(vendorSrc,['vendors']);
     gulp.watch(['build/*.html', 'build/assets/css/*.css', 'build/assets/js/*.js', 'build/assets/js/vendors/*.js']).on('change', browserSync.reload);
 
@@ -96,4 +127,7 @@ gulp.task('watch', function(){
 
 
 // use default task to launch Browsersync and watch JS files
-gulp.task('default', [ 'sass', 'images', 'scripts', 'vendors', 'watch'], function () {});
+gulp.task('default', [ 'sass', 'scripts', 'vendors', 'watch'], function () {});
+
+
+//watchify
